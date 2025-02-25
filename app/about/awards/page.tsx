d0c1c9a15c706a50427e27c84b4bb8ff42af810f
Rama Aditya Maulana <ramaadtym@gmail.com>
2025-02-25 16:01:15 +0700

    add border award page

--- app/about/awards/page.tsx
+++ app/about/awards/page.tsx
@@ -5,6 +5,7 @@ import {ContentType} from "@/types/indes";
 import {Metadata} from "next";
 import {notFound} from "next/navigation";
 import React from "react";
+import {cn} from "@/lib/utils";
 
 export async function generateMetadata(): Promise<Metadata> {
   const result: ContentType = await getData();
@@ -48,6 +49,8 @@ const getData = async () => {
 
 const page = async () => {
   const data: ContentType = await getData();
+  const columns = 4
+  const totalRows = Math.ceil(data?.images?.length / columns);
 
   return (
     <section>
@@ -59,12 +62,20 @@ const page = async () => {
         <h1 className="title-3 text-green-light text-center">{data.title}</h1>
         <div className="mt-8" dangerouslySetInnerHTML={{__html: data.description}}></div>
 
-        <section className="grid grid-cols-2 mt-8 lg:mt-16 md:grid-cols-4 gap-8">
-          {data?.images?.map((d) => (
-            <div key={d._id} className="flex justify-center">
-              <img className="w-full object-contain" src={d?.images[0]?.url} alt={d?.title} />
-            </div>
-          ))}
+        <section className="grid grid-cols-2 mt-8 lg:mt-16 md:grid-cols-4">
+          {data?.images?.map((d, index) => {
+              const isLastRow = Math.floor(index / columns) === totalRows - 1;
+              return(
+                <div key={d._id} 
+                className={cn("flex justify-center border-r-2 last:border-r-0 border-[#005CAB]", 
+                {
+                  "border-b-2": !isLastRow
+                })}>
+                  <img className="w-full h-full object-contain p-2" src={d?.images[0]?.url} alt={d?.title} />
+              </div>
+              )
+            }
+          )}
         </section>
       </section>
     </section>
