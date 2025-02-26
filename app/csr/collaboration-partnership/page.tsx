1cb4071ecd9744797ee61a48198bc84ad9df375c
andriputra <andriputra.job@gmail.com>
2025-02-27 04:14:48 +0700

    change simage for relateg page

--- app/csr/collaboration-partnership/page.tsx
+++ app/csr/collaboration-partnership/page.tsx
@@ -53,13 +53,13 @@ const CollaborationPartnership = async () => {
   const linksData = [
     {
       href: "/csr",
-      image: data?.banner[0]?.images[0]?.url,
+      image: "/temp/csr.png",
       alt: "Tanggung Jawab Sosial",
       title: "Tanggung Jawab Sosial",
     },
     {
       href: "/csr/our-programs",
-      image: data?.banner[0]?.images[0]?.url,
+      image: "temp/program-kami.png",
       alt: "Program Kami",
       title: "Program Kami",
     },
