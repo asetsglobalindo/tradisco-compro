34b4e5028d24711fcc13038d6f14af5560c52172
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2025-04-11 11:36:02 +0700

    update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -66,14 +66,22 @@ const Footer = () => {
                   <h3 className="font-medium">Follow Us :</h3>
                 </div>
                 <div className="flex space-x-4">
-                  <a href="https://www.instagram.com/tradisco.id" target="_blank" className="hover:opacity-80">
+                  <a
+                    href="https://www.instagram.com/tradisco.id"
+                    target="_blank"
+                    className="hover:opacity-80"
+                  >
                     <img
                       className="w-4 h-4"
                       src="https://cdn.jsdelivr.net/npm/simple-icons@v8/icons/instagram.svg"
                       alt="Instagram"
                     />
                   </a>
-                  <a href="https://www.linkedin.com/company/trading-digital-and-construction" target="_blank" className="hover:opacity-80">
+                  <a
+                    href="https://www.linkedin.com/company/trading-digital-and-construction"
+                    target="_blank"
+                    className="hover:opacity-80"
+                  >
                     <img
                       className="w-4 h-4"
                       src="https://cdn.jsdelivr.net/npm/simple-icons@v8/icons/linkedin.svg"
@@ -103,16 +111,15 @@ const Footer = () => {
                   HSSE
                 </Link>
                 <Link
-                  href="/laporan-tahunan"
-                  className="text-gray-600 hover:text-blue-600"
+                  href="/company-profile"
+                  className="text-gray-600 hover:text-blue-600 flex items-center"
                 >
-                  Laporan Tahunan
-                </Link>
-                <Link
-                  href="/kemitraan"
-                  className="text-gray-600 hover:text-blue-600"
-                >
-                  Kemitraan
+                  Company Profile
+                  <img
+                    src="/icons/download.png"
+                    alt="Download"
+                    className="w-4 h-4 ml-2"
+                  />
                 </Link>
               </div>
             </div>
@@ -122,39 +129,45 @@ const Footer = () => {
               <div className="flex items-center mb-6">
                 <h2 className="text-xl font-bold">Contact Us</h2>
               </div>
-              <div className="flex flex-col space-y-4">
-                <div className="flex items-start">
-                  <img
-                    src="/icons/location2.png"
-                    alt="Location"
-                    className="w-5 h-5 mt-1 mr-3"
-                  />
-                  <span className="text-gray-600">
+              <div className="flex flex-col space-y-6">
+                <div className="flex items-start group">
+                  <div className="w-8 h-8 flex items-center justify-center mr-4">
+                    <img
+                      src="/icons/location2.png"
+                      alt="Location"
+                      className="w-4 h-4"
+                    />
+                  </div>
+                  <span className="text-gray-600 mt-1">
                     Menara Palma Jl. H. R. Rasuna Said, DKI Jakarta 12950
                   </span>
                 </div>
-                <div className="flex items-center">
-                  <img
-                    src="/icons/call2.png"
-                    alt="Phone"
-                    className="w-5 h-5 mr-3"
-                  />
+                <div className="flex items-center group">
+                  <div className="w-8 h-8 flex items-center justify-center mr-4">
+                    <img
+                      src="/icons/call2.png"
+                      alt="Phone"
+                      className="w-4 h-4"
+                    />
+                  </div>
                   <a
                     href="tel:0895-4046-02222"
-                    className="text-gray-600 hover:text-blue-600"
+                    className="text-gray-600 group-hover:text-blue-600 transition-colors"
                   >
                     0895-4046-02222
                   </a>
                 </div>
-                <div className="flex items-center">
-                  <img
-                    src="/icons/mail2.png"
-                    alt="Email"
-                    className="w-5 h-5 mr-3"
-                  />
+                <div className="flex items-center group">
+                  <div className="w-8 h-8 flex items-center justify-center mr-4 transition-colors">
+                    <img
+                      src="/icons/mail2.png"
+                      alt="Email"
+                      className="w-4 h-4"
+                    />
+                  </div>
                   <a
                     href="mailto:team@tradisco.co.id"
-                    className="text-gray-600 hover:text-blue-600"
+                    className="text-gray-600 group-hover:text-blue-600 transition-colors"
                   >
                     team@tradisco.co.id
                   </a>
