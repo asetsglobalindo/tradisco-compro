ef1f177ccc57421703bfc0dcb429f59a2ea678a7
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2025-04-29 15:12:00 +0700

    ✨ feat: setup google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,18 @@
-import type {Metadata} from "next";
+import type { Metadata } from "next";
 import "./globals.scss";
 import "leaflet/dist/leaflet.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import ReactQueryClientProvider from "@/components/ReactQueryClientProvider";
 import ProgressBar from "@/components/ProgressBar";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Tradisco Global Inovasi",
   description: "",
   icons: "/favicon.ico",
 };
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -18,6 +20,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-TG1YPERRSE"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-TG1YPERRSE');
+          `}
+        </Script>
+      </head>
       <ReactQueryClientProvider>
         <body>
           <Header />
@@ -29,4 +45,3 @@ export default function RootLayout({
     </html>
   );
 }
-
